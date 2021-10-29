5e52733c3558bbbbadcc82474e70d3698e28e3d1
Antti Takalahti <antti.takalahti@blueprintgenetics.com>
2021-10-29 11:25:26 +0300

    added multi person gate

--- gate.go
+++ gate.go
@@ -11,12 +11,6 @@ type Gate struct {
     mu sync.Mutex
 }
 
-var gateInstance *Gate
-
-func init() {
-    gateInstance = new(Gate)
-}
-
 func (gate Gate) Enter(id string) {
     gateInstance.mu.Lock()
     defer gateInstance.mu.Unlock()
@@ -26,15 +20,40 @@ func (gate Gate) Enter(id string) {
     fmt.Println("person with id: " + id + " passed through")
 }
 
+type MultiPersonGate struct {
+
+}
+
+func (multiPersonGate MultiPersonGate) Enter(id string) {
+    fmt.Println("    person with id: " + id + " is at the multi person gate")
+    time.Sleep(100 * time.Millisecond)
+    fmt.Println("    person with id: " + id + " passed through the multi person gate")
+}
+
+var gateInstance *Gate
+var multiPersonGateInstance *MultiPersonGate
+
+func init() {
+    gateInstance = new(Gate)
+    multiPersonGateInstance = new (MultiPersonGate)
+}
+
 func enter(n int) {
     gate := Gate{}
     gate.Enter(strconv.Itoa(n))
 }
 
+func enterMulti(n int) {
+    multiPersonGate := MultiPersonGate{}
+    multiPersonGate.Enter(strconv.Itoa(n))
+}
+
 func main() {
     for i := 0; i < 10; i++ {
         go enter(i)
+        go enterMulti(i)
     }
+
     var input string
     fmt.Scanln(&input)
 }
